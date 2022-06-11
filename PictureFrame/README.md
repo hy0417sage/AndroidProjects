b6c81bdcad7e93c28355752ce48bd03856b01b7d
송현영 <hy0417sage@gmail.com>
2022-06-11 23:00:16 +0900

    add design UI, permission
    add design UI, permission

---
+++ PictureFrame/README.md
@@ -0,0 +1,12 @@
+# 전자액자
+- Layout 을 그리는 법
+  - 가로로 화면 그리기
+- [Android Permission](https://developer.android.com/training/permissions/requesting?hl=ko) 사용하기
+- View Animation 사용하기
+- Activity Lifecycle(안드로이드 생명주기)
+- Content Provider : 앱 외부에서 사진 가져오는 법
+  - SAF (Storage Access Framework)
+
+### 전자액자
+- 저장소 접근 권한을 이용하여 로컬 사진을 로드할 수 있음
+- 추가한 사진들을 일정한 간격으로 전환하여 보여줄 수 있음
--- PictureFrame/app/src/main/java/com/hy0417sage/androidprojects/pictureframe/MainActivity.kt
+++ PictureFrame/app/src/main/java/com/hy0417sage/androidprojects/pictureframe/MainActivity.kt
@@ -1,11 +1,69 @@
 package com.hy0417sage.androidprojects.pictureframe
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val addPhotoButton: Button by lazy {
+        findViewById<Button>(R.id.addPhotoButton)
+    }
+
+    private val startPictureFrame: Button by lazy {
+        findViewById<Button>(R.id.startPictureFrame)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initAddPhotoButton() //
+        initStartPhotoFrameModeButton()
+    }
+
+    private fun initAddPhotoButton() {
+        addPhotoButton.setOnClickListener {
+            //권한 체크
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    //TODO 권한이 잘 부여되었을 때 갤러리에서 사진을 선택하는 기능
+                }
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                    // 교육용 팝업 확인 후 권한 팝업을 띄우는 기능
+                    showPermissionContextPopup()
+                }
+                else -> {
+                    requestPermissions(
+                        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                        1000
+                    )
+                }
+            }
+        }
+    }
+
+    private fun showPermissionContextPopup() {
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요합니다.")
+            .setMessage("전자액자 앱에서 사진을 불러오기 위해 권한이 필요합니다.")
+            .setPositiveButton("동의하기") { _, _ ->
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    1000)
+            }
+            .setNegativeButton("취소하기") { _, _ -> }
+            .create()
+            .show()
+    }
+
+    private fun initStartPhotoFrameModeButton() {
+
     }
 }
